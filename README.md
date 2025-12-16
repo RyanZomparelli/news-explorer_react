a2f4c572b5b717b6afc6b16da4eeefaf7c51b1d0
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-12-15 19:02:20 -0500

    Scaffold project with vite, set up directories and fonts, add normalize.css and add demo photos to README.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# news-explorer_react
+# News Explorer
+
+## About the project
+
+### About News Explorer...
+
+- [Figma design](https://www.figma.com/design/3ottwMEhlBt95Dbn8dw1NH/Your-Final-Project?node-id=22618-1012&t=2VvlmAPvt3527zuo-0)
+
+## Table of contents
+
+## Screenshots
+
+### Headers (Mobile & Desktop)
+
+<div style="display: flex; gap: 10px; justify-content: center;">
+
+  <div style="flex: 1; text-align: center;">
+    <p><strong>Mobile Header</strong></p>
+    <img src="./src/assets/demo/header-mobile.png" width="100%" />
+  </div>
+
+  <div style="flex: 1; text-align: center;">
+    <p><strong>Desktop Header</strong></p>
+    <img src="./src/assets/demo/demo_desktop_logged-out.png" width="100%" />
+  </div>
+
+</div>
+
+### Saved Articles (Desktop)
+
+<div style="text-align: center;">
+  <img src="./src/assets/demo/demo_desktop_logged-in.png" width="100%" />
+</div>
---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+// New 'flat-config' for eslint VS the older legacy .eslintrc.cjs
+
+export default [
+  // Block 1: Ignore the dist folder so build output doesn’t get linted.
+  { ignores: ["dist"] },
+  // Block 2: For all *.js/*.jsx files, in a browser React app, with hooks, apply these rules/plugins/language settings.
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
