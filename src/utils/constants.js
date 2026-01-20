dde483fac80279a8990bfd9ac9525ab74a759299
Ryan Zomparelli <zomp.ryan@gmail.com>
2026-01-20 08:58:09 -0500

    update frontend .env setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 // 3rd PARTY NEWS API
-export const newsApiKey = "b0bff49aa9f54201aa752a3ec410e898";
+export const newsApiKey = import.meta.env.VITE_NEWS_API_KEY;
 
 // For development.
 export const newsApiUrl = "https://newsapi.org/v2/everything";
 
 // For production. The proxy url is necessary because the newsApi is only free in development.
-export const proxyApiUrl = "https://nomoreparties.co/news/v2/everything";
+export const proxyApiUrl = import.meta.env.VITE_PROXY_API_URL;
 
 // NODE_ENV is an env varaiable automatically set by Vite. When you run the build
 // script it's set to production. Otherwise it's value is development.
